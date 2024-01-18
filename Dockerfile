e13ea40955ab61f4e28b47a67dcc42c59a0addbf
Ciro <cironidas@gmail.com>
2024-01-18 10:03:37 +0100

    i

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.8
+RUN pip install pandas scikit-learn streamlit
+COPY src/app.py /app/
+WORKDIR /app
+
+ENTRYPOINT ["streamlit", "run", "app.py"]
+
---
+++ src/app.py
@@ -0,0 +1,55 @@
+import streamlit as st
+
+url_imagen = "https://img.freepik.com/vector-gratis/diagrama-celula-vastago-fondo-blanco_1308-15286.jpg"
+
+st.image(url_imagen)
+
+st.title("¡Hola neurona!")
+
+
+tab1, tab2, tab3 = st.tabs(["Una entrada", "Dos entradas", "Tres entradas"])
+
+with tab3:
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        w0 = st.slider("Peso w₀", min_value=0.0, max_value=5.0, value=2.5, key="w0_t3")
+        x0 = st.number_input("Entrada x₀", min_value=2.0, max_value=3.0, value=2.5, format="%f", key="x0_t3")
+    with col2:
+        w1 = st.slider("Peso w₁", min_value=0.0, max_value=5.0, value=2.5, key="w1_t3")
+        x1 = st.number_input("Entrada x₁", min_value=2.0, max_value=3.0, value=2.5, format="%f", key="x1_t3")
+    with col3:
+        w2 = st.slider("Peso w₂", min_value=0.0, max_value=5.0, value=2.5, key="w2_t3")
+        x2 = st.number_input("Entrada x₂", min_value=2.0, max_value=3.0, value=2.5, format="%f", key="x2_t3")
+    b = st.number_input("Introduzca el sesgo", key="b_t3")
+    boton_calcular_salida_t3 = st.button("Calcular salida", key="boton_t3")
+
+    if boton_calcular_salida_t3:
+        y = x0 * w0 + x1 * w1 + x2 * w2 + b
+        st.write("Salida:", y)
+
+with tab2:
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        w0 = st.slider("Peso w₀", min_value=0.0, max_value=5.0, value=2.5, key="w0_t2")
+        x0 = st.number_input("Entrada x₀", min_value=2.0, max_value=3.0, value=2.5, format="%f", key="x0_t2")
+    with col2:
+        w1 = st.slider("Peso w₁", min_value=0.0, max_value=5.0, value=2.5, key="w1_t2")
+        x1 = st.number_input("Entrada x₁", min_value=2.0, max_value=3.0, value=2.5, format="%f", key="x1_t2")
+
+    boton_calcular_salida_t2 = st.button("Calcular salida", key="boton_t2")
+
+    if boton_calcular_salida_t2:
+        y = x0 * w0 + x1 * w1
+        st.write("Salida:", y)
+
+with tab1:
+    w = st.slider("Peso w", min_value=0.0, max_value=5.0, value=2.5, key="w_t1")
+    x = st.number_input("Entrada x", min_value=2.0, max_value=3.0, value=2.5, format="%f", key="x_t1")
+    boton_calcular_salida_t1 = st.button("Calcular salida", key="boton_t1")
+
+    if boton_calcular_salida_t1:
+        y = x * w
+        st.write("Salida:", y)
